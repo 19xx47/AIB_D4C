5d97742c85bd089131e9b3c6837d7044cc9dc143
D4C <115446911+13D4C@users.noreply.github.com>
2023-06-10 22:54:28 +0700

    app.py

--- app.py
+++ app.py
@@ -1,52 +1,22 @@
-import streamlit as st
-import torch
-from detect import detect
-from PIL import Image
-from io import *
 import glob
-from datetime import datetime
-import os
+import streamlit as st
 import wget
+from PIL import Image
+import torch
+import cv2
+import os
 import time
 
+st.set_page_config(layout="wide")
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/best.pt', force_reload=True) 
-
-def imageInput(device, src):
-    
-    if src == 'อัปโหลดรูปภาพ':
-        image_file = st.file_uploader("ตรวจสอบรูปภาพ", type=['png', 'jpeg', 'jpg'])
-        col1, col2 = st.columns(2)
-        if image_file is not None:
-            img = Image.open(image_file)
-            with col1:
-                st.image(img, caption='รูปภาพที่นำเข้ามา', use_column_width='always')
-            ts = datetime.timestamp(datetime.now())
-            imgpath = os.path.join('data/uploads', str(ts)+image_file.name)
-            outputpath = os.path.join('data/outputs', os.path.basename(imgpath))
-            with open(imgpath, mode="wb") as f:
-                f.write(image_file.getbuffer())
-
-            #call Model prediction--
-            
-            model.cuda() if device == 'cuda' else model.cpu()
-            pred = model(imgpath)
-            pred.render()  # render bbox in image
-            for im in pred.ims:
-                im_base64 = Image.fromarray(im)
-                im_base64.save(outputpath)
-
-            #--Display predicton
-            
-            img_ = Image.open(outputpath)
-            with col2:
-                st.image(img_, caption='ผลลัพธ์จากการตรวจสอบ', use_column_width='always')
-
-    
+cfg_model_path = 'best.pt'
+model = None
+confidence = .25
 
 
 
-def video_input(device, src):
+
+def video_input(data_src):
     vid_file = None
     if data_src == 'Sample data':
         vid_file = "data/sample_videos/sample.mp4"
@@ -100,54 +70,107 @@ def video_input(device, src):
 
         cap.release()
 
-def main():
-    # -- Sidebar
-    st.sidebar.title('🧠 Face Recognition')
-    datasrc = st.sidebar.radio("เลือกประเภทรูปแบบการนำเข้า", ['อัปโหลดรูปภาพ'])
-    
-        
-                
-    option = st.sidebar.radio("ระบุประเภทข้อมูล", ['Image', 'Video'], disabled = False)
-    if torch.cuda.is_available():
-        deviceoption = st.sidebar.radio("ประมวลผลโดยใช้", ['cpu', 'cuda'], disabled = False, index=1)
+
+def infer_image(img, size=None):
+    model.conf = confidence
+    result = model(img, size=size) if size else model(img)
+    result.render()
+    image = Image.fromarray(result.ims[0])
+    return image
+
+
+@st.experimental_singleton
+def load_model(path, device):
+    model_ = torch.hub.load('ultralytics/yolov5', 'custom', path=path, force_reload=True)
+    model_.to(device)
+    print("model to ", device)
+    return model_
+
+
+@st.experimental_singleton
+def download_model(url):
+    model_file = wget.download(url, out="models")
+    return model_file
+
+
+def get_user_model():
+    model_src = st.sidebar.radio("Model source", ["file upload", "url"])
+    model_file = None
+    if model_src == "file upload":
+        model_bytes = st.sidebar.file_uploader("Upload a model file", type=['pt'])
+        if model_bytes:
+            model_file = "models/uploaded_" + model_bytes.name
+            with open(model_file, 'wb') as out:
+                out.write(model_bytes.read())
     else:
-        deviceoption = st.sidebar.radio("ประมวลผลโดยใช้", ['cpu', 'cuda'], disabled = True, index=0)
-    # -- End of Sidebar
+        url = st.sidebar.text_input("model url")
+        if url:
+            model_file_ = download_model(url)
+            if model_file_.split(".")[-1] == "pt":
+                model_file = model_file_
 
-    st.header('👷 Ai Face Recognitation')
-    
-    if option == "Image":    
-        imageInput(deviceoption, datasrc)
+    return model_file
+
+def main():
+    # global variables
+    global model, confidence, cfg_model_path
+
+    st.title("Object Recognition Dashboard")
+
+    st.sidebar.title("Settings")
+
+    # upload model
+    model_src = st.sidebar.radio("Select yolov5 weight file", ["Use our demo model 5s", "Use your own model"])
+    # URL, upload file (max 200 mb)
+    if model_src == "Use your own model":
+        user_model_path = get_user_model()
+        if user_model_path:
+            cfg_model_path = user_model_path
+
+        st.sidebar.text(cfg_model_path.split("/")[-1])
+        st.sidebar.markdown("---")
+
+    # check if model file is available
+    if not os.path.isfile(cfg_model_path):
+        st.warning("Model file not available!!!, please added to the model folder.", icon="⚠️")
+    else:
+        # device options
+        if torch.cuda.is_available():
+            device_option = st.sidebar.radio("Select Device", ['cpu', 'cuda'], disabled=False, index=0)
+        else:
+            device_option = st.sidebar.radio("Select Device", ['cpu', 'cuda'], disabled=True, index=0)
+
+        # load model
+        model = load_model(cfg_model_path, device_option)
+
+        # confidence slider
+        confidence = st.sidebar.slider('Confidence', min_value=0.1, max_value=1.0, value=.45)
+
+        # custom classes
+        if st.sidebar.checkbox("Custom Classes"):
+            model_names = list(model.names.values())
+            assigned_class = st.sidebar.multiselect("Select Classes", model_names, default=[model_names[0]])
+            classes = [model_names.index(name) for name in assigned_class]
+            model.classes = classes
+        else:
+            model.classes = list(model.names.keys())
+
+        st.sidebar.markdown("---")
+
+        # input options
+        input_option = st.sidebar.radio("Select input type: ", ['image', 'video'])
+
+        # input src option
+        data_src = st.sidebar.radio("Select input source: ", ['Sample data', 'Upload your own data'])
+
+        if input_option == 'image':
+            video_input(data_src)
+        else:
+            video_input(data_src)
 
 
-        
-        # valuesimg = st.slider('Show test Image', 0, 3, 0)
-
-        # if(valuesimg == 0):
-        #     st.image(image1, caption='picture 1')
-        #     st.write("ผลลัพท์การตรวจสอบ")
-        # elif(valuesimg == 1):
-        #     st.image(image2, caption='picture 2')
-        #     st.write("ผลลัพท์การตรวจสอบ")
-        # elif(valuesimg == 2):
-        #     st.image(image3, caption='picture 3')
-        #     st.write("ผลลัพท์การตรวจสอบ")
-    elif option == "Video": 
-        videoInput(deviceoption, datasrc)
-
-       
-
-
-    
-    
-
-if __name__ == '__main__':
-  
- main()
-@st.cache
-def loadModel():
-    start_dl = time.time()
-    model_file = "models/best.pt" 
-    finished_dl = time.time()
-    print(f"Model Downloaded, ETA:{finished_dl-start_dl}")
-loadModel()
+if __name__ == "__main__":
+    try:
+        main()
+    except SystemExit:
+        pass
