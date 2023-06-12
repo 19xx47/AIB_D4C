8f840e0bf9899462c48de6b951462bd58cac9480
Worakan Lasudee <78127819+19xx47@users.noreply.github.com>
2023-06-12 20:25:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,11 @@ from datetime import datetime
 import os
 import wget
 import time
-
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/best.pt', force_reload=True) 
+import cv2
+cfg_model_path = 'best.pt'
+model = None
+confidence = .25
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt', force_reload=True) 
 
 def imageInput(device, src):
     
@@ -25,7 +28,6 @@ def imageInput(device, src):
             outputpath = os.path.join('data/outputs', os.path.basename(imgpath))
             with open(imgpath, mode="wb") as f:
                 f.write(image_file.getbuffer())
-
             #call Model prediction--
             
             model.cuda() if device == 'cuda' else model.cpu()
@@ -45,52 +47,128 @@ def imageInput(device, src):
 
 
 
-def videoInput(device, src):
-    uploaded_video = st.file_uploader("Upload Video", type=['mp4', 'mpeg', 'mov'])
-    if uploaded_video != None:
 
-        ts = datetime.timestamp(datetime.now())
-        pp = ts
-        imgpath = os.path.join('data/uploads', str(ts)+uploaded_video.name)
-        outputpath = os.path.join('data/video_output', os.path.basename(imgpath))
+def video_input(data_src):
+    vid_file = None
+    if data_src == 'Sample data':
+        vid_file = "data/sample_videos/sample.mp4"
+    else:
+        vid_bytes = st.sidebar.file_uploader("Upload a video", type=['mp4', 'mpv', 'avi'])
+        if vid_bytes:
+            vid_file = "data/uploaded_data/upload." + vid_bytes.name.split('.')[-1]
+            with open(vid_file, 'wb') as out:
+                out.write(vid_bytes.read())
 
-        with open(imgpath, mode='wb') as f:
-            f.write(uploaded_video.read())  # save video to disk
+    if vid_file:
+        cap = cv2.VideoCapture(vid_file)
+        custom_size = st.sidebar.checkbox("Custom frame size")
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        if custom_size:
+            width = st.sidebar.number_input("Width", min_value=120, step=20, value=width)
+            height = st.sidebar.number_input("Height", min_value=120, step=20, value=height)
 
-        st_video = open(imgpath, 'rb')
-        video_bytes = st_video.read()
-        st.video(video_bytes)
-        st.write("วีดีโอที่ถูกนำเข้ามา")
-        detect(weights="models/best.pt", source=imgpath, device=0,project=outputpath) if device == 'cuda' else detect(weights="models/best.pt", source=imgpath, device='cpu',project=outputpath)
-        st_video2 = open(outputpath+"/exp/"+ str(pp)+uploaded_video.name, 'rb')
-        video_bytes2 = st_video2.read()
-        st.video(video_bytes2)
-        st.write("ผลลัพท์การตรวจสอบ")
-        # st.video(video_bytes2)
-        st.download_button(label="Download video file", data=video_bytes2,file_name='video_clip.mp4')
+        fps = 0
+        st1, st2, st3 = st.columns(3)
+        with st1:
+            st.markdown("## Height")
+            st1_text = st.markdown(f"{height}")
+        with st2:
+            st.markdown("## Width")
+            st2_text = st.markdown(f"{width}")
+        with st3:
+            st.markdown("## FPS")
+            st3_text = st.markdown(f"{fps}")
+
+        st.markdown("---")
+        output = st.empty()
+        prev_time = 0
+        curr_time = 0
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                st.write("Can't read frame, stream ended? Exiting ....")
+                break
+            frame = cv2.resize(frame, (width, height))
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            output_img = infer_image(frame)
+            output.image(output_img)
+            curr_time = time.time()
+            fps = 1 / (curr_time - prev_time)
+            prev_time = curr_time
+            st1_text.markdown(f"**{height}**")
+            st2_text.markdown(f"**{width}**")
+            st3_text.markdown(f"**{fps:.2f}**")
+
+        cap.release()
        
-        
+def infer_image(img, size=None):
+    model.conf = confidence
+    result = model(img, size=size) if size else model(img)
+    result.render()
+    image = Image.fromarray(result.ims[0])
+    return image 
+      
+@st.experimental_singleton
+def load_model(path, device):
+    model_ = torch.hub.load('ultralytics/yolov5', 'custom', path=path, force_reload=True)
+    model_.to(device)
+    print("model to ", device)
+    return model_
 
+@st.experimental_singleton
+def download_model(url):
+    model_file = wget.download(url, out="models")
+    return model_file
+
+def get_user_model():
+    model_src = st.sidebar.radio("Model source", ["file upload", "url"])
+    model_file = None
+    if model_src == "file upload":
+        model_bytes = st.sidebar.file_uploader("Upload a model file", type=['pt'])
+        if model_bytes:
+            model_file = "models/uploaded_" + model_bytes.name
+            with open(model_file, 'wb') as out:
+                out.write(model_bytes.read())
+    else:
+        url = st.sidebar.text_input("model url")
+        if url:
+            model_file_ = download_model(url)
+            if model_file_.split(".")[-1] == "pt":
+                model_file = model_file_
+
+    return model_file
 
 def main():
     # -- Sidebar
     st.sidebar.title('🧠 Face Recognition')
     datasrc = st.sidebar.radio("เลือกประเภทรูปแบบการนำเข้า", ['อัปโหลดรูปภาพ'])
-    
-        
                 
     option = st.sidebar.radio("ระบุประเภทข้อมูล", ['Image', 'Video'], disabled = False)
-    if torch.cuda.is_available():
-        deviceoption = st.sidebar.radio("ประมวลผลโดยใช้", ['cpu', 'cuda'], disabled = False, index=1)
-    else:
-        deviceoption = st.sidebar.radio("ประมวลผลโดยใช้", ['cpu', 'cuda'], disabled = True, index=0)
+
     # -- End of Sidebar
 
     st.header('👷 Ai Face Recognitation')
-    
-    if option == "Image":    
-        imageInput(deviceoption, datasrc)
+    if not os.path.isfile(cfg_model_path):
+        st.warning("Model file not available!!!, please added to the model folder.", icon="⚠️")
+    else:
+        # device options
+        if torch.cuda.is_available():
+            device_option = st.sidebar.radio("Select Device", ['cpu', 'cuda'], disabled=False, index=1)
+        else:
+            device_option = st.sidebar.radio("Select Device", ['cpu', 'cuda'], disabled=True, index=0)
 
+        # load model
+        model = load_model(cfg_model_path, device_option)
+
+        # confidence slider
+        confidence = st.sidebar.slider('Confidence', min_value=0.1, max_value=1.0, value=.45)
+
+        data_src = st.sidebar.radio("Select input source: ", ['Sample data', 'Upload your own data'])
+        if option == "Image":    
+            imageInput(device_option, datasrc)
+        else:
+            video_input(data_src)
 
         
         # valuesimg = st.slider('Show test Image', 0, 3, 0)
@@ -110,14 +188,15 @@ def main():
 
     
     
-
-if __name__ == '__main__':
-  
-    main()
+if __name__ == "__main__":
+    try:
+        main()
+    except SystemExit:
+        pass
 @st.cache
 def loadModel():
     start_dl = time.time()
-    model_file = "models/best.pt" 
+    model_file = "best.pt" 
     finished_dl = time.time()
     print(f"Model Downloaded, ETA:{finished_dl-start_dl}")
 loadModel()
